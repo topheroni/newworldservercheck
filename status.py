4d9934996d6ee0add2cc8ac79849902ce7cd783d
Christopher Rutherford <chris36787@gmail.com>
2023-05-07 13:36:04 -0400

    fix final status check

--- status.py
+++ status.py
@@ -21,45 +21,46 @@ def main(server: str = "Aaru"):
         server_list = soup.find_all(
             "div", class_="ags-ServerStatus-content-responses-response-server"
         )
-        server_names = soup.find_all(
-            "div", class_="ags-ServerStatus-content-responses-response-server-name"
-        )
+        nw_servers = get_servers(soup)
         last_update = soup.find(
             class_="ags-ServerStatus-content-lastUpdated"
         ).text.strip()
         server_index = -1
-        nw_servers = [server.text.strip() for server in server_names]
         for i, _ in enumerate(server_list):
             if server in nw_servers:
                 server_index = i
         server_info = server_list[server_index]
         server_statuses = get_status(server_info)
         if server_index == -1:
-            message = (
+            print(
                 f"{server} is not a valid server name. "
                 f"Please ensure the name matches *exactly* what is shown on the server list ({url})."
             )
-            print(message)
             full = False
+            val_resp = False
         if "Full" in server_statuses:
             print(
                 f"{server} is full as of {last_update}.\n"
                 "Automatically checking again in 30 seconds."
             )
+            val_resp = True
             time.sleep(30)
         if "Online" in server_statuses:
             print(f"{server} has room!")
             full = False
+            val_resp = True
         if "Maintenance" in server_statuses:
             print(
                 f"{server} is under maintenance as of {last_update}.\n"
                 "Automatically checking again in 60 seconds."
             )
+            val_resp = True
             time.sleep(60)
         if "Character transfer is unavailable" in server_statuses:
             print("Character transfer is currently unavailable for this server.")
             full = False
-        else:
+            val_resp = True
+        if not val_resp:
             print(
                 f"Unexpected response for server status(es): "
                 + ", ".join(server_statuses)
@@ -68,7 +69,17 @@ def main(server: str = "Aaru"):
         print("—" * 60)
 
 
-def get_status(server_status):
+def get_servers(soup: BeautifulSoup) -> list[str]:
+    """Get the list of active New World servers."""
+    server_names = soup.find_all(
+        "div",
+        class_="ags-ServerStatus-content-responses-response-server-name",
+    )
+    return [server.text.strip() for server in server_names]
+
+
+def get_status(server_status: element.Tag):
+    """Get the status(es) of the requested New World server."""
     server_status_classes = []
     for tag in server_status:
         if isinstance(tag, element.Tag):
